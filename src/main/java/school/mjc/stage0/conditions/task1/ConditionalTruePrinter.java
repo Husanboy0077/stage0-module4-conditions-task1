eea56c7f7024521c33c1354ac8f9b38668b9a17a
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-29 15:23:40 +0500

    yes

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task1;
-
+import java.util.Scanner;
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+
+        int x = 5, y = 5;
+        if(x>=y){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("true");
+        }
+        
     }
 }
